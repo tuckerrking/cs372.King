7d88ef94456af46be41e7ed45d8018e075fb7198
Tucker King <tuckerrking@gmail.com>
2021-09-14 09:15:04 -0500

    Created cs372Stack.h

---
+++ include/cs372Stack.h
@@ -0,0 +1,24 @@
+#include "cs372List.h"
+
+template <typename T>
+  
+class Stack :: Container
+{
+private:
+  List<T> *stackList; = new List<T>();
+public:
+  Stack() {}
+  Stack(Stack &rhs( {}
+  ~Stack() {delete stackList;}
+  bool empty() {return stackList.empty();}
+  void push(T data) {stackList.push_front(data);}
+  T pop() {return stackList.pop_front()}
+  void traverse(void (*dolt)(T data)){
+    stackList.traverse(dolt);
+  };
+  size_t size() override;
+  size_t max_size() override;
+  bool empty() override;
+  void swap(Container &, Container &) override;
+  void clear() override;
+}
