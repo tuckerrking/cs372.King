01da7853f26f7cc96ef886a8ca62c2a9815e025f
Tucker King <tuckerrking@gmail.com>
2021-09-20 08:54:31 -0500

    Updated cs372List.h
    Updated formatting

--- include/cs372List.h
+++ include/cs372List.h
@@ -1,116 +1,167 @@
+//
+//  cs372List.h
+//  cs372_headers
+//
+//  Created by Tucker King on 9/20/21.
+//
+
+#ifndef cs372List_h
+#define cs372List_h
 #pragma once
+
 #include <iostream>
-template <typename T>
+
+using namespace std;
+
+template<typename T>
 class List
 {
 private:
-  class Node {
-  public:
-    T  data;
-    Node* prev;
-    Node* next;
-  };
-  Node* head = nullptr;
-  Node* tail = nullptr;
-  void setupList() {
-    Node* newNode = new Node();
-    newNode->next = nullptr;
-    newNode->prev = nullptr;
-    head = newNode;
-    tail = newNode;
-  }
-  void deleteListContents() {
-    Node* temp = nullptr;
-    Node* current = head;
-    while (current != nullptr) {
-      temp = current->next;
-      delete current;
-      current = temp;
+    class Node
+    {
+    public:
+        T data;
+        Node *prev;
+        Node *next;
+    };
+    Node *head= nullptr;
+    Node *tail= nullptr;
+    
+    void setupList()
+    {
+        Node *newNode= new Node();
+        newNode->next= nullptr;
+        newNode->prev= nullptr;
+        head= newNode;
+        tail= newNode;
     }
-  }
+    
+    void deleteListContents()
+    {
+        Node *temp= nullptr;
+        Node *current= head;
+        while(current!= nullptr)
+        {
+            temp= current->next;
+            delete current;
+            current= temp;
+        }
+    }
 public:
-  List() : head(nullptr), tail(nullptr)  {}
-  
-  List(T newData) {
-    setupList();
-    head->data = newData;
-  }
-  List(List& rhs) { // copy constructor
-    deleteListContents();
-    head = rhs.head;
-    tail = rhs.tail;
-  }
-  ~List() {// And a destructor
-    deleteListContents();
-  }
-  bool  empty() {
-    return (head == nullptr);
-  }
-  void push_front(T data) {
-    Node* newNode = new Node();
-    newNode->data = data;
-    newNode->next = head;
-    newNode->prev = nullptr;
-    if (empty()) {
-      head = newNode;
-      tail = newNode;
+    List() : head(nullptr), tail(nullptr) {}
+    
+    List(T newData)
+    {
+        setUpList();
+        head->data= newData;
     }
-    else {
-        head->prev = newNode; 
-        head = newNode;
+    
+    List(List& rhs) //Copy constructor
+    {
+        deleteListContents();
+        head= rhs.head;
+        tail= rhs.tail;
     }
-  }
-  void push_back(T data) {
-    Node* newNode = new Node();
-    newNode->data = data;
-    newNode->next = nullptr;
-    newNode->prev = tail;
-    if (empty()) {
-      tail = newNode;
-      head = newNode;
+    
+    ~List() //Destructor
+    {
+        deleteListContents();
     }
-    else {
-      tail->next = newNode;
-      tail = newNode;
+    
+    bool empty()
+    {
+        return(head== nullptr);
     }
-  }
-  void pop_back() {
-    Node *lastNode = tail;
-    if (lastNode != nullptr) {
-      tail = tail->prev;
-      tail->next = nullptr;
-      delete lastNode;
+    
+    void push_front(T data)
+    {
+        Node *newNode = new Node();
+        newNode->data= data;
+        newNode->next= head;
+        newNode->prev= nullptr;
+        
+        if(empty())
+        {
+            head= newNode;
+            tail= newNode;
+        }
+        else
+        {
+            head->prev= newNode;
+            head= newNode;
+        }
     }
-  }
-  T front() {
-    if (!empty()) {
-      return head->data;
+    
+    void push_back(T data)
+    {
+        Node *newNode= new Node();
+        newNode->data= data;
+        newNode->next= nullptr;
+        newNode->prev= tail;
+        
+        if(empty())
+        {
+            tail= newNode;
+            head= newNode;
+        }
+        else
+        {
+            tail->next= newNode;
+            tail= newNode;
+        }
     }
-    else {
-      // This is drastic, and should be handled using an exception handler
-      std::cout << "Exception: list is empty."  << std::endl;
-      exit(1);
+    
+    void pop_back()
+    {
+        Node *lastNode= tail;
+        
+        if(lastNode!= nullptr)
+        {
+            tail= tail->prev;
+            tail->next= nullptr;
+            delete lastNode;
+        }
     }
-  }
-  T back() {
-    if (!empty()) {
-      return tail->data;
+    
+    T front()
+    {
+        if(!empty())
+        {
+            return head->data;
+        }
+        else
+        {
+            cout << "Exception: list is empty." << endl;
+            exit(1);
+        }
     }
-    else {
-      // This is drastic, and should be handled using an exception handler
-      std::cout << "Exception: list is empty."  << std::endl;
-      exit(1);
+    
+    T back()
+    {
+        if(!empty())
+        {
+            return tail->data;
+        }
+        else
+        {
+            cout << "Exception: list is empty." << endl;
+            exit(1);
+        }
     }
-  }
-  void traverse(void (*doIt)(T data)) {
-    Node* current = head;
-    while (current != nullptr) {
-      doIt(current->data);
-      current = current->next;
+    
+    void traverse(void(*doIt)(T data))
+    {
+        Node *current= head;
+        
+        while(current!= nullptr)
+        {
+            doIt(current->data);
+            current= current->next;
+        }
     }
-  }
 };
 
+#endif /* cs372List_h */
 
 
 
